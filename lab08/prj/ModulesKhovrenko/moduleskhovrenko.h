3587d3c930b85553b6be515130142c75c3a23821
kyoshy <x-e-d@ukr.net>
2021-05-24 17:33:14 +0300

    Updated moduleskhovrenko.h

--- lab08/prj/ModulesKhovrenko/moduleskhovrenko.h
+++ lab08/prj/ModulesKhovrenko/moduleskhovrenko.h
@@ -9,8 +9,8 @@ struct task_9_1
     double pay = 0;
 };
 
-task_9_1 Bonus9_1 (double price);
-float Temperature9_2 (float t);
+task_9_1 Bonus9_1(double price);
+float Temperature9_2(float t);
 int Binary9_3(int N);
 
 #endif // MODULESKHOVRENKO_H
